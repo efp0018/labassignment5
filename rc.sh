3726c895b2ea662542dd228959f89fbc22af2f9b
allyswank <ars0151@auburn.edu>
2021-10-06 14:59:19 -0500

    added sample.fa and ran the reverse compliment of the sequence

--- rc.sh
+++ rc.sh
@@ -1,34 +1,37 @@
 #! /bin/sh
 
-#initialize variable
-reverse=""
-
+#ALLY SWANK DID THIS PART WITH HER BEST EFFORTS
 #read in data
-name=`grep ">" $1`
-sequence=`grep -v ">" $1`
-
+name=`grep ">" sample.fa`
+sequence=`grep -v ">" sample.fa`
 echo "Name: $name"
 
 #calculate length of sequence
 len=${#sequence}
-
 echo "Length: $len"
 
 #loop through sequence in reverse
-for (( i=$len; i>=0; i-- ))
-do
-    reverse="$reverse${sequence:$i:1}"
-done
+#for (( i=$len; i>=0; i-- ))
+#do
+#    reverse="$reverse${sequence:$i:1}"
+#done
 
-echo "$sequence
-$reverse
-"
+#ALLY SWANK DID THIS PART WITH HER BEST EFFORTS
+#Replace the loop with a one-liner 
+#converts sequence string to the reverse complement of that sequence
+#Remove any code that is no longer necessary to output the reverse complement of a sequence string
+echo "$sequence" | rev
+echo "Sequence: $sequence"
+
+#echo "$sequence
+#$reverse
+#"
 
 #complement sequence
-rc=`echo $reverse | tr 'atcg' 'tagc'`
+#rc=`echo $reverse | tr 'atcg' 'tagc'`
 
-echo "$rc
-"
+#echo "$rc
+#"
 
-echo $name >$1.rc.txt
-echo $rc >>$1.rc.txt
+#echo $name >$1.rc.txt
+#echo $rc >>$1.rc.txt
